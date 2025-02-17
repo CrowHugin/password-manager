5532bd0e9399d41c658735c453c9c3ccbc027d1f
CrowHugin <kuntz.bastien@gmail.com>
2025-02-17 13:26:46 +0100

    add some tests - need to be changed

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+
+import unittest
+import main
+from main import users_input, randomizing_password
+
+#phase testing
+class Test_result:
+    def input_test(self):
+        test = "ui","google"
+        print(test)
+        # self.assertEqual()
+
+if __name__ == "__main__":
+    Test_result.input_test()
