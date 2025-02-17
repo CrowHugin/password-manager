9dcff86d09c21a4b963b6ee6fc8839fd58c40ecc
CrowHugin <kuntz.bastien@gmail.com>
2025-02-17 10:07:54 +0100

    add random content

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import string
+import random
+
+letters = string.ascii_letters
+
 def users_input():
 
     password = input("rentrez votre mot de passe\n")
     website = input("rentrez le site associé svp\n")
     return password,website
 
+def randomizing_password(password, letters):
+    for i in password:
+        
+        one_letter = random.choice(letters)
+        password = password.replace(i,one_letter)
+    return password
+
 
 if __name__ == "__main__":
 
     password,website = users_input()
+    coded_password=randomizing_password(password,letters)
 
 print(f"the password is {password}, the associated website is {website}")
+print(f"the coded password is {coded_password}, the associated website is {website}")
