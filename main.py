a50a5c0b2f1ceb8b856e5506c5d135540123a687
CrowHugin <kuntz.bastien@gmail.com>
2025-06-03 22:59:13 +0200

    renamed oop by ressources

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
 # from ressources import users, password, code,stockage
-import ressources.ressources as rs
-
+from ressources.ressources import password, view
+import argparse
 import string
 printable = string.printable
 
+
+def parsing():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-view",
+                        help = "to view a password with the email or website",
+                        action = "store_true")
+
+    parser.add_argument("-add",
+                        help = "to add an email and a password",
+                        action = "store_true")
+
+    parser.add_argument("-create",
+                        help = "to view smth",
+                        action = "store_true")
+
+    parser.add_argument("-email",
+                        help = "to put an email",
+                        action = "store")
+
+    parser.add_argument("-password",
+                        help = "to put a password",
+                        action = "store")
+
+
+
+    arguments = parser.parse_args()
+    return arguments
 #main function
 if __name__ == "__main__":
-   rs.users.choices_users(printable)
+    args = parsing()
+    if args.view:
+        view.viewing(printable, args.email)
+  
+    if args.add:
+        password.put_password(printable, args.email, args.password)
+
+    if args.create:
+        print("create")
