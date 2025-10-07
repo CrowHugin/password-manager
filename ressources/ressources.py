995fcc040289cc6c99effec3336931beb7debd98
CrowHugin <kuntz.bastien@gmail.com>
2025-10-07 18:57:35 +0200

    Changed ressources.py to conform to tests

--- ressources/ressources.py
+++ ressources/ressources.py
@@ -1,21 +1,19 @@
 #! /bin/python3
 
-import string
 import random
 import sys
 import csv
 import os
 
 
-class password:
+class password():
+    @staticmethod
     def put_password(printable, email, mdp, website):
         coded_password=code.coding_password(mdp,printable)
         stockage.stockage(coded_password,email,website)
 
-class users:
-
-
-
+class users():
+    @staticmethod
     def create(printable,lenght):
         liste = []
         i = 0
@@ -25,68 +23,75 @@ class users:
 
         return liste
 
+    @staticmethod
     def save_info():
-        loop = True
         ans = input("Would you like to save it ?\nY or N\n")
 
-        while loop:
-            if ans == "Y" or ans == "y":
-                eml = input("With which email adress?\n")
-                wbsti = input("on which website?\n")
-                loop = False
-                return eml, wbsti
+        if ans in ("Y","y"):
+            eml = input("With which email adress?\n")
+            wbsti = input("on which website?\n")
 
-            elif ans == "N" or ans == "n":
-                print("Please ensure to copy the password")
-                return None, None
-                loop = False
+        elif ans in ("N","n"):
+            print("Please ensure to copy the password")
+            eml = None
+            wbsti = None
 
-            else:
-                ans = input("Please be sure to put an answer\n")
+        else:
+            print("Please be sure to put a correct answer")
+            sys.exit()
+
+        return eml, wbsti
 
 class code():
-    def coding_password(password, table):
+    @staticmethod
+    def coding_password(passwrd, table):
         coded_password_list = []
-        for cara in password:
+        for cara in passwrd:
             if cara in table:
                 ind = table.index(cara)
                 coded_password_list.append(table[ind+2])
         coded_password = "".join(coded_password_list)
         return coded_password
 
+    @staticmethod
     def decoding_password(coded_password,printable):
         user_password = coded_password
         for i in coded_password:
             if i in printable:
                 ind = printable.index(i)
                 new_cara = printable[ind-2]
-            user_password = user_password.replace(i,new_cara)
+                user_password = user_password.replace(i,new_cara)
         return user_password
-    
-class stockage:
+
+class stockage():
+    @staticmethod
     def stockage(mdp, email, website):
         csv_file = os.path.join(os.path.expanduser('~'),'password-manager')
 
         if os.path.exists(csv_file):
-            with open(f"{csv_file}/stock.csv", 'a', newline='', encoding="UTF-8") as file:
+            with open(f"{csv_file}/stock.csv", 'a', newline='',
+                      encoding="UTF-8") as file:
                 file.write(f"\n{mdp},{email},{website}")
 
         if not os.path.exists(csv_file):
             os.system(f"mkdir -p {csv_file}")
 
-            with open(f"{csv_file}/stock.csv", 'w', newline='', encoding="UTF-8") as file:
+            with open(f"{csv_file}/stock.csv", 'w', newline='',
+                      encoding="UTF-8") as file:
                 writer = csv.writer(file, delimiter=',')
                 writer.writerow(["mdp", "email", "website"])
                 file.write(f"\n{mdp},{email},{website}")
-                
-                
 
-                
-class view():    
+
+
+
+class view():
+    @staticmethod
     def read_file(file, variable, prtable,wnt_srch):
         liste = []
-        with open(f"{file}/stock.csv", 'r', newline='',encoding="UTF-8") as file:
-            for i in file:
+        with open(f"{file}/stock.csv", 'r', newline='',
+                  encoding="UTF-8") as f:
+            for i in f:
                 if variable in i:
                     split = i.split(",")
                     passwrd = split[0]
@@ -107,27 +112,26 @@ class view():
                 print(j)
 
 
-
+    @staticmethod
     def viewing(table, email, website):
         csv_file = os.path.join(os.path.expanduser('~'),'password-manager')
-        if not os.path.exists("stockage"): #maybe need to change it later
+        if not os.path.exists(csv_file): #maybe need to change it later
             print("ERROR: make sure to have a stockage file")
             sys.exit()
         else:
             #if email isn't put
             if email == "pass":
-                var = website  
-                search = "website"            
-           
+                var = website
+                search = "website"
+
             #if website isn't put
             elif website == "pass":
                 var = email
                 search = "email"
- 
+
             #if both of them are put
             else:
                 var = f"{email},{website}"
                 search = "both"
 
             view.read_file(csv_file, var, table, search)
-
